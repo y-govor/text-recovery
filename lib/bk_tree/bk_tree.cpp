3b268d15779ef087492a7a48345d49000070a5b0
y-govor <y-govor@proton.me>
2025-08-04 03:07:31 +0300

    Add BKTree class implementation

---
+++ lib/bk_tree/bk_tree.cpp
@@ -0,0 +1,63 @@
+/*
+    Text Recovery
+    A C++ program for restoring damaged text
+    Copyright (C) 2025 Yurii Govor
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "bk_tree.h"
+#include "bk_tree_node.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+/**
+ * @brief Default constructor
+ *
+ */
+BKTree::BKTree() : root(nullptr) {}
+
+/**
+ * @brief Insert a word into a tree
+ *
+ * @param word A word to insert
+ */
+void BKTree::insert(const std::string& word)
+{
+    // Check if root node was initialized (has a word)
+    if(this->root == nullptr)
+    {
+        this->root = std::make_unique<BKTreeNode>();
+        this->root.get()->setWord(word);
+        return;
+    }
+
+    this->root.get()->insert(word);
+}
+
+/**
+ * @brief Find all words similar to the query within the tolerance value
+ *
+ * @param query Query
+ * @param tolerance Tolerance value (max edit distance)
+ * @return A list of matching words
+ */
+std::vector<std::string> BKTree::find(const std::string& query, unsigned int tolerance) const
+{
+    std::vector<std::string> results;
+    this->root.get()->find(query, tolerance, results);
+    return results;
+}
