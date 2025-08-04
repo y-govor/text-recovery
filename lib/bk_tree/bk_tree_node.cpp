e14442862d111b26378bb0909b11aab3c17fb09a
y-govor <y-govor@proton.me>
2025-08-04 03:06:45 +0300

    Add BKTreeNode class implementation

---
+++ lib/bk_tree/bk_tree_node.cpp
@@ -0,0 +1,141 @@
+/*
+    Text Recovery
+    A C++ program for restoring damaged text
+    Copyright (C) 2025 Yurii Govor
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "bk_tree_node.h"
+#include "edit_distance.h"
+
+#include <memory>
+
+/**
+ * @brief Default constructor
+ *
+ */
+BKTreeNode::BKTreeNode() : word(), children() {}
+
+/**
+ * @brief Get the word of the current node
+ *
+ * @return A word of the current node
+ */
+std::string BKTreeNode::getWord() const
+{
+    return this->word;
+}
+
+/**
+ * @brief Set a word for the current node
+ *
+ * @param word A word of the current node
+ */
+void BKTreeNode::setWord(const std::string& word)
+{
+    this->word = word;
+}
+
+/**
+ * @brief Check if a node has a child node with the specified edit distance
+ *
+ * @param distance Edit distance of the child node
+ * @return true if a child node with the specified edit distance exists, otherwise false
+ */
+bool BKTreeNode::hasChild(int distance) const
+{
+    auto it = this->children.find(distance);
+    // If the item does not exist, the iterator will point at the end
+    return it != this->children.end();
+}
+
+/**
+ * @brief Get a child node with the specified edit distance
+ *
+ * @param distance Edit distance
+ * @return A child node with the specified edit distance
+ */
+BKTreeNode* BKTreeNode::getChild(int distance) const
+{
+    auto it = this->children.find(distance);
+    return (it != this->children.end()) ? it->second.get() : nullptr;
+}
+
+/**
+ * @brief Add a new child node
+ *
+ * @param distance Edit distance
+ * @param word A word of the node
+ */
+void BKTreeNode::addChild(int distance, const std::string& word)
+{
+    if(!this->hasChild(distance))
+    {
+        this->children[distance] = std::make_unique<BKTreeNode>();
+        this->children[distance].get()->setWord(word);
+    }
+}
+
+/**
+ * @brief Insert a word
+ *
+ * @param word A word to insert
+ */
+void BKTreeNode::insert(const std::string& word)
+{
+    BKTreeNode* node = this;
+
+    while(true)
+    {
+        int distance = EditDistance::editDistance(word, node->getWord());
+
+        if(!node->hasChild(distance))
+        {
+            node->addChild(distance, word);
+            return;
+        }
+
+        node = node->getChild(distance);
+    }
+}
+
+/**
+ * @brief Find all words similar to the query within the tolerance value
+ *
+ * @param query Query
+ * @param tolerance Tolerance value (max edit distance)
+ * @param results A collection with results to pass
+ */
+void BKTreeNode::find(const std::string& query, unsigned int tolerance,
+                      std::vector<std::string>& results) const
+{
+    int distance = EditDistance::editDistance(query, this->word);
+
+    if(distance <= tolerance)
+    {
+        results.push_back(this->word);
+    }
+
+    int min_dist = distance - tolerance;
+    int max_dist = distance + tolerance;
+
+    for(const auto& it : this->children)
+    {
+        if(it.first >= min_dist && it.first <= max_dist)
+        {
+            it.second.get()->find(query, tolerance, results);
+        }
+    }
+}
---
+++ lib/bk_tree/bk_tree_node.h
@@ -0,0 +1,109 @@
+/*
+    Text Recovery
+    A C++ program for restoring damaged text
+    Copyright (C) 2025 Yurii Govor
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef BK_TREE_NODE_H_INCLUDED
+#define BK_TREE_NODE_H_INCLUDED
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+/**
+ * @brief Class that represents a node in a BK-tree
+ *
+ */
+class BKTreeNode
+{
+private:
+    /**
+     * @brief A word of the current node
+     *
+     */
+    std::string word;
+    /**
+     * @brief Children of the current node. The collection stores an edit distance between
+     * the word of this node and the child node, as well as a pointer to that node
+     *
+     */
+    std::unordered_map<int, std::unique_ptr<BKTreeNode>> children;
+
+public:
+    /**
+     * @brief Default constructor
+     *
+     */
+    BKTreeNode();
+
+    /**
+     * @brief Get the word of the current node
+     *
+     * @return A word of the current node
+     */
+    std::string getWord() const;
+    /**
+     * @brief Set a word for the current node
+     *
+     * @param word A word of the current node
+     */
+    void setWord(const std::string& word);
+
+    /**
+     * @brief Check if a node has a child node with the specified edit distance
+     *
+     * @param distance Edit distance of the child node
+     * @return true if a child node with the specified edit distance exists, otherwise false
+     */
+    bool hasChild(int distance) const;
+
+    /**
+     * @brief Get a child node with the specified edit distance
+     *
+     * @param distance Edit distance
+     * @return A child node with the specified edit distance
+     */
+    BKTreeNode* getChild(int distance) const;
+
+    /**
+     * @brief Add a new child node
+     *
+     * @param distance Edit distance
+     * @param word A word of the node
+     */
+    void addChild(int distance, const std::string& word);
+
+    /**
+     * @brief Insert a word
+     *
+     * @param word A word to insert
+     */
+    void insert(const std::string& word);
+
+    /**
+     * @brief Find all words similar to the query within the tolerance value
+     *
+     * @param query Query
+     * @param tolerance Tolerance value (max edit distance)
+     * @param results A collection with results to pass
+     */
+    void find(const std::string& query, unsigned int tolerance,
+              std::vector<std::string>& results) const;
+};
+
+#endif // BK_TREE_NODE_H_INCLUDED
